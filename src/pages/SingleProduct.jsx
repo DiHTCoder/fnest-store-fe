d01bda60995d2a10d66a0b90203dc0cba40a906d
Huỳnh Tiến Dĩ <103420884+DiHTCoder@users.noreply.github.com>
2023-11-22 23:49:08 +0700

    Single product page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,32 +1,63 @@
-import { useLoaderData } from 'react-router-dom';
 import { formatPrice } from '../utils/helpers';
 import { useState } from 'react';
-import { customFetch } from '../services';
 import { AiOutlineHeart } from 'react-icons/ai';
-import { Stars, ProductsTab, Breadcrumb } from '../components';
-import { HiMiniMinus } from 'react-icons/hi2';
-import { HiMiniPlus } from 'react-icons/hi2';
-
-export const loader = async ({ params }) => {
-    const response = await customFetch(`/products/${params.id}`);
-    return { product: response.data.data };
-};
+import { Stars, ProductsTab, Breadcrumb, Loading } from '../components';
+import { useParams } from 'react-router-dom';
+import productServices from '../services/productServices';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { addItemToCart, getTotals } from '../features/cart/cartSlice';
+import { addItemToFavourite } from '../features/favourite/favouriteSlice';
 
 const SingleProduct = () => {
-    const { product } = useLoaderData();
-    const { image, title, price, description, colors, company } = product.attributes;
-    const vndPrice = formatPrice(price);
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    const [isLoading, setIsLoading] = useState(false);
+    const [product, setProduct] = useState('');
     const [amount, setAmount] = useState(1);
-    const increase = () => {
+    const { name, price, description, size, material, sold, inStock, featured, categoryId, collectionId, thumbnail } =
+        product;
+
+    useEffect(() => {
+        const fetchProductById = async () => {
+            setIsLoading(true);
+            try {
+                const response = await productServices.getProductDetail(id);
+                setProduct(response.data);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Error fetching room:', error);
+                setIsLoading(false);
+            }
+        };
+        fetchProductById();
+    }, [id]);
+
+    const handleAddToFavourite = () => {
+        dispatch(addItemToFavourite(product));
+    };
+    const handleAddToCart = () => {
+        dispatch(addItemToCart({ ...product, quantity: amount }));
+        dispatch(getTotals());
+    };
+    const handleAmountChange = (event) => {
+        const inputAmount = parseInt(event.target.value, 10);
+
+        if (!isNaN(inputAmount) && inputAmount >= 1 && inputAmount <= inStock) {
+            setAmount(inputAmount);
+        }
+    };
+    const handleIncrease = () => {
         setAmount((oldAmount) => {
             let tempAmount = oldAmount + 1;
-            // if (tempAmount > stock) {
-            //   tempAmount = stock
-            // }
+            if (tempAmount > inStock) {
+                tempAmount = inStock;
+            }
             return tempAmount;
         });
     };
-    const decrease = () => {
+
+    const handleDecrease = () => {
         setAmount((oldAmount) => {
             let tempAmount = oldAmount - 1;
             if (tempAmount < 1) {
@@ -40,72 +71,89 @@ const SingleProduct = () => {
             <Breadcrumb url="products" page="Chi tiết sản phẩm" />
             <div className="grid grid-cols-2 gap-10">
                 <div>
-                    <img src={image} alt="" />
+                    <img src={thumbnail} alt={name} />
                 </div>
                 <div>
                     <div className="flex justify-between">
-                        <h2 className="text-4xl font-bold">{title}</h2>
+                        <h2 className="text-4xl font-bold">{name}</h2>
                         <AiOutlineHeart className="w-[30px] h-[30px] text-primary transition-colors duration-300" />
                     </div>
 
                     <div className="flex justify-between p-2">
-                        <div>
-                            {' '}
+                        <div className="flex">
                             <Stars />
                             (100 đánh giá)
                         </div>
                         <span>
-                            <b>Đã bán: </b>1
+                            <b>Đã bán: </b>
+                            {sold}
                         </span>
                     </div>
-                    <div className="text-lg  ">
-                        <span className="text-primary text-2xl font-bold pr-4">{vndPrice}</span>
-                        <span className="line-through">11111000 đ</span>
+                    <div className="text-lg">
+                        <span className="text-primary text-2xl font-bold pr-4">{formatPrice(price)}</span>
+                        <span className="line-through">{formatPrice(price)}</span>
                     </div>
                     <div className="pb-2 border-b-2">
                         <span className="text-sm tex-base-300">SKU:{product.id}</span>
                     </div>
                     <p className="py-2">
                         <b>Chất liệu: </b>
-                        Gỗ cao su tự nhiên
+                        {material}
                     </p>
                     <p className="py-2">
                         <b>Kích thước: </b>
-                        Dài 200cm x Rộng 92cm x Cao 78cm
+                        {size}
                     </p>
                     <p className="py-2">
                         <b>Số lượng còn lại: </b>
-                        30
+                        {inStock}
                     </p>
                     <p className="py-2 leading-loose">
                         <b>Mô tả: </b>
                         {description}
                     </p>
-                    <p className="py-2">
-                        <b>Công ty: </b>
-                        {company}
-                    </p>
+
                     <div className="mt-3">
                         <label htmlFor="count" className="text-paragraph font-semibold text-base-content-300">
                             Số lượng
                         </label>
-                        <div className="my-2">
-                            <button className="border-2 w-[50px] h-[50px]">
-                                <HiMiniMinus className="mx-4" />
+                        <div class="flex items-center border-gray-100 my-2">
+                            <button
+                                class="btn btn-ghost"
+                                onClick={() => {
+                                    handleDecrease(amount);
+                                }}
+                            >
+                                -
                             </button>
                             <input
-                                id="amount"
-                                value={amount}
+                                class="h-10 w-10 border bg-white text-center text-xs outline-none rounded-lg"
                                 type="text"
-                                class="border-2 w-[50px] h-[50px] text-center"
-                            ></input>
-                            <button className="border-2 w-[50px] h-[50px]">
-                                <HiMiniPlus className="mx-4" />
+                                value={amount}
+                                min="1"
+                                max={inStock}
+                                inputmode="decimal"
+                                onChange={handleAmountChange}
+                            />
+                            <button
+                                class="btn btn-ghost"
+                                onClick={() => {
+                                    handleIncrease(amount);
+                                }}
+                            >
+                                +
                             </button>
                         </div>
                         <div className="flex gap-5">
-                            <button className="btn btn-ghost bg-primary text-white">Thêm vào giỏ hàng</button>
-                            <button className="btn btn-ghost bg-accent-focus text-white">Mua ngay</button>
+                            <button className="btn btn-ghost bg-primary text-white" onClick={() => handleAddToCart()}>
+                                Thêm vào giỏ hàng
+                            </button>
+                            <button
+                                className="btn btn-ghost bg-accent-focus text-white"
+                                onClick={() => handleAddToFavourite()}
+                            >
+                                Thêm vào wishlist
+                            </button>
                         </div>
                     </div>
                 </div>
